a4114548fc7be26a471d600a2f2d4db739cb563f
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2021-05-31 10:05:01 -0400

    week 4

--- problem_loader.py
+++ problem_loader.py
@@ -6,19 +6,29 @@ def preprocess_data(data):
     return list(map(int, data.split())) 
 
 class ProblemLoader():
-  def __init__(self, url, preprocessor=preprocess_data):
+  def __init__(self, url, fname="100kint.p", preprocessor=preprocess_data):
     self.url = url
     self.preprocess_data = preprocessor
+    self.fname = fname
 
-  def fetch(self):
+  def fetch(self, clear=False):
     values = []
+
+    if clear:
+        logging.debug("Ignoring any existing dataset!")
+        logging.info("Preprocess new dataset")
+        r = requests.get(self.url, allow_redirects=True)
+        values = self.preprocess_data(r.content)
+        pickle.dump(values, open(self.fname, "wb"))
+        return values
+        
     try:
         logging.info("Loading existing dataset")
-        values = pickle.load(open("100kint.p", "rb"))
+        values = pickle.load(open(self.fname, "rb"))
     except:
         logging.debug("Failed to load existing dataset!")
         logging.info("Preprocess new dataset")
         r = requests.get(self.url, allow_redirects=True)
         values = self.preprocess_data(r.content)
-        pickle.dump(values, open("100kint.p", "wb"))
+        pickle.dump(values, open(self.fname, "wb"))
     return values
